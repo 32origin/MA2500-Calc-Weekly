f85401da387f27c0371dd64bd705586f8ac5b8b3
32origin <88385708+32origin@users.noreply.github.com>
2022-05-07 01:39:22 +0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -59,6 +59,14 @@ def main():
             else:
                 judge="*1.2"
             tilt="今日沪指高于"+judge+"线"
+            if judge =="÷1.2":
+                tilt = tilt + "稳定持有定投中"
+                else if judge == "MA2500":
+                    tilt = tilt + "准备到*1.2线停止定投"
+                    else if judge =="*1.2":
+                        tilt = tilt + "终止定投，随时准备跑路"
+                
+               
         print(tilt)
         #GENERATE TITLE
         # 斜杠用来代码换行
